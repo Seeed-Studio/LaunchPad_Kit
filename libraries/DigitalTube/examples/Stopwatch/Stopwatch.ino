cb6a2fe13eb25217514cabf19a7206986486ebd8
ewakefield <e-wakefield@ti.com>
2015-02-21 16:21:48 -0600

    Updated Stopwatch.ino to work with MSP430F5529
    Original used TimerOne.h which does not work with the LaunchPad ecosystem. 
    
    Stores up to 31 time saves.

--- libraries/DigitalTube/examples/Stopwatch/Stopwatch.ino
+++ libraries/DigitalTube/examples/Stopwatch/Stopwatch.ino
@@ -29,9 +29,7 @@
  * THE SOFTWARE.
  */
 
-#include <EEPROM.h>
-#include <TimerOne.h>
-#include <avr/pgmspace.h>
+#include <MspFlash.h>
 #include "TM1637.h"
 #define ON 1
 #define OFF 0
@@ -43,11 +41,20 @@ unsigned char microsecond_10 = 0;
 unsigned char second;
 unsigned char _microsecond_10 = 0;
 unsigned char _second;
-unsigned int eepromaddr;
+volatile unsigned int store_index = 0;
+
 boolean Flag_ReadTime;
 
-#define CLK 2//pins definitions for TM1637 and can be changed to other ports
-#define DIO 3
+// Flash Infomem segment used in lieu of an EEPROM
+#define FLASH_INFOB_SEGMENT __attribute__((section(".infob")))
+#define MAX_FLASH_ENTRIES 31
+FLASH_INFOB_SEGMENT volatile unsigned int flash_count = 0;
+FLASH_INFOB_SEGMENT volatile unsigned char flash_microsecond_10[MAX_FLASH_ENTRIES];
+FLASH_INFOB_SEGMENT volatile unsigned char flash_second[MAX_FLASH_ENTRIES];
+
+
+#define CLK 39 //pins definitions for TM1637 and can be changed to other ports
+#define DIO 38
 TM1637 tm1637(CLK,DIO);
 
 void setup()
@@ -55,13 +62,15 @@ void setup()
   Serial.begin(9600);
   tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
   tm1637.init();
-  Timer1.initialize(10000);//timing for 10ms
-  Timer1.attachInterrupt(TimingISR);//declare the interrupt serve routine:TimingISR
+  TimerA0_initialize(10);//timing for 10ms
+  store_index = flash_count;  // store_index points to newest entry written to flash
+
   Serial.println("Please send command to control the stopwatch:");
   Serial.println("S - start");
   Serial.println("P - pause");
   Serial.println("L - list the time");
-  Serial.println("W - write the time to EEPROM ");
+  Serial.println("W - write the time to Flash");
+  Serial.println("E - Erase the Flash");
   Serial.println("R - reset");
 }
 void loop()
@@ -74,6 +83,7 @@ void loop()
     case 'P':stopwatchPause();Serial.println("Stopwatch was paused");break;
     case 'L':readTime();break;
     case 'W':saveTime();Serial.println("Save the time");break;
+    case 'E':eraseTime();Serial.println("All stopwatch entries erased from flash");break;
     case 'R':stopwatchReset();Serial.println("Stopwatch was reset");break;
     default:break;
   }
@@ -84,6 +94,25 @@ void loop()
   }
 }
 //************************************************
+
+// Configure hardware Timer_A instance 0 to interrupt every <X> milliseconds using the
+// board's 32768Hz crystal
+void TimerA0_initialize(unsigned int ms)
+{
+  unsigned long timer_rollover_value = (ms * 32768) / 1000;
+  
+  // Init Timer_A0
+  TA0CTL = TACLR;
+  TA0CTL = TASSEL__ACLK | ID__1 | MC__STOP;
+  // Configure Count Compare Register 0 - the value at which our timer IRQ will fire (see below)
+  TA0CCR0 = (uint16_t) timer_rollover_value;
+  TA0CCTL0 = CCIE; // Enable CCR0 interrupt
+  
+  // We're leaving the timer stopped here since the program manages that start/stop condition itself
+}
+
+// Hardware Timer_A instance 0's CCR0 interrupt
+__attribute__((interrupt(TIMER0_A0_VECTOR)))
 void TimingISR()
 {
   microsecond_10 ++;
@@ -113,14 +142,14 @@ void TimeUpdate(void)
   TimeDisp[1] = _second % 10;
   Update = OFF;
 }
-void stopwatchStart()//timer1 on
+void stopwatchStart()// Start Timer_A0 counting
 {
   Flag_ReadTime = 0;
-  TCCR1B |= Timer1.clockSelectBits;
+  TA0CTL |= MC__UP;
 }
-void stopwatchPause()//timer1 off if [CS12 CS11 CS10] is [0 0 0].
+void stopwatchPause()// Stop Timer_A0
 {
-  TCCR1B &= ~(_BV(CS10) | _BV(CS11) | _BV(CS12));
+  TA0CTL = TA0CTL & ~(MC_1 | MC_2 | MC_3);  // Clear all MC_* bits, meaning MC__STOP
 }
 void stopwatchReset()
 {
@@ -134,22 +163,67 @@ void stopwatchReset()
 }
 void saveTime()
 {
-  EEPROM.write(eepromaddr ++,microsecond_10);
-  EEPROM.write(eepromaddr ++,second);
+  unsigned char save_microsecond_10[MAX_FLASH_ENTRIES], save_second[MAX_FLASH_ENTRIES];
+  unsigned int save_flash_count = flash_count;
+  int i;
+
+  if (flash_count == MAX_FLASH_ENTRIES) {
+    Serial.println("Can't save any more entries! Flash full.");
+    return;
+  }
+
+  // Save entire contents of flash into memory first
+  for (i=0; i < MAX_FLASH_ENTRIES; i++) {
+    save_microsecond_10[i] = flash_microsecond_10[i];
+    save_second[i] = flash_second[i];
+  }
+
+  // Update in-memory arrays with new microsecond_10 and second values
+  save_microsecond_10[save_flash_count] = microsecond_10;
+  save_second[save_flash_count] = second;
+
+  // Erase flash segment
+  Flash.erase((unsigned char *)&flash_count);
+
+  // Write flash_count to flash, incrementing it
+  save_flash_count++;
+  Flash.write((unsigned char *)&flash_count, (unsigned char *)&save_flash_count, sizeof(flash_count));
+
+  // Write saved copy of microsecond_10[] array and second[] array to their flash backing store
+  Flash.write((unsigned char *)flash_microsecond_10, (unsigned char *)save_microsecond_10, sizeof(flash_microsecond_10));
+  Flash.write((unsigned char *)flash_second, (unsigned char *)save_second, sizeof(flash_second));
+
+  // Update store_index to point to the (new) end of the array
+  store_index = flash_count;
 }
 void readTime()
 {
   Flag_ReadTime = 1;
-  if(eepromaddr == 0)
+  if(flash_count == 0)
   {
-    Serial.println("The time had been read");
+    Serial.println("The time has never been written to flash");
     _microsecond_10 = 0;
     _second = 0;
   }
-  else{
-  _second = EEPROM.read(-- eepromaddr);
-  _microsecond_10 = EEPROM.read(-- eepromaddr);
-  Serial.println("List the time");
+  else {
+    _second = flash_second[store_index-1]; // Reading from MSP430 Infomem Flash is easy!
+    _microsecond_10 = flash_microsecond_10[store_index-1];
+    Serial.println("List the time");
+    store_index--;
+    if (store_index == 0) {
+      store_index = flash_count;  // Roll back to beginning
+      Serial.println("Last entry shown");
+    }
   }
   Update = ON;
+}
+void eraseTime()
+{
+  // Purge Flash infomem contents
+  Flash.erase((unsigned char *)&flash_count);
+
+  // Reinitialize flash_count and store_index to 0.
+  unsigned int new_flash_count = 0;
+  Flash.write((unsigned char *)&flash_count, (unsigned char *)&new_flash_count, sizeof(flash_count));
+  store_index = flash_count;
 }
